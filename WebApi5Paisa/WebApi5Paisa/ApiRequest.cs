8892d70da10b7808981b9c5c1d64815755c38b68
SarveshInnovatiacs <Sarvesh@innovatiacs.com>
2024-04-15 21:17:10 +0530

    commit

---
+++ WebApi5Paisa/WebApi5Paisa/ApiRequest.cs
@@ -0,0 +1,402 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace WebApi5Paisa
+{
+    public class ApiRequest
+    {
+        public static string SendApiRequest(string url, string Request)
+        {
+            string strresponse = "";
+            try
+            {
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+                httpWebRequest.ContentType = "application/json";
+
+                httpWebRequest.Method = "POST";
+
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    if (!string.IsNullOrEmpty(Request))
+                        streamWriter.Write(Request);
+                }
+                using (var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse())
+                {
+                    using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                    {
+                        strresponse = streamReader.ReadToEnd();
+                    }
+
+                    if (httpResponse.StatusCode != HttpStatusCode.OK)
+                    {
+                        throw new Exception(string.Format("Server error (HTTP {0}: {1}).", httpResponse.StatusCode, httpResponse.StatusDescription));
+                    }
+
+                    string[] reponseURI = httpResponse.Headers.AllKeys;
+                    string CookieName = httpResponse.Headers.Get("Set-Cookie");
+
+                    SetCookies(CookieName);
+                }
+
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+
+            return strresponse;
+
+        }
+        #region Testing Api Only
+        public static string SendApiRequestTOTPLogin(string url, string Request)
+        {
+            string strresponse = "";
+            try
+            {
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+                httpWebRequest.ContentType = "application/json";
+
+                httpWebRequest.Method = "POST";
+
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    if (!string.IsNullOrEmpty(Request))
+                        streamWriter.Write(Request);
+                }
+                using (var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse())
+                {
+                    using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                    {
+                        strresponse = streamReader.ReadToEnd();
+                    }
+
+                    if (httpResponse.StatusCode != HttpStatusCode.OK)
+                    {
+                        throw new Exception(string.Format("Server error (HTTP {0}: {1}).", httpResponse.StatusCode, httpResponse.StatusDescription));
+                    }
+
+                    
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return strresponse;
+
+        }
+        public static string SendApiRequestGetOuthLogin(string url, string Request)
+        {
+            string strresponse = "";
+            try
+            {
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+                httpWebRequest.ContentType = "application/json";
+
+                httpWebRequest.Method = "POST";
+
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    if (!string.IsNullOrEmpty(Request))
+                        streamWriter.Write(Request);
+                }
+                using (var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse())
+                {
+                    using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                    {
+                        strresponse = streamReader.ReadToEnd();
+
+
+                        var agr = JsonConvert.DeserializeObject<TokenResponse>(strresponse);
+                        var reqtok = agr.body.AccessToken;
+
+
+                        string path = Path.Combine(Directory.GetCurrentDirectory(), @"AuthToken\AuthKey.txt");
+
+                        if (File.Exists(path))
+                        {
+                            TextWriter tw = new StreamWriter(path, false);
+                            tw.Write(string.Empty);
+                            tw.Close();
+
+                            using (StreamWriter writer = new StreamWriter(path))
+                            {
+                                writer.WriteLine(reqtok);
+                            }
+                        }
+                    }
+                   
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return strresponse;
+
+        }
+        public static string SendApiRequestHolding(string url, string Request, string Type = "Openapi")
+        {
+            string Json = "";
+            string BearerToken = "";
+            try
+            {
+                string path = Path.Combine(Directory.GetCurrentDirectory(), @"AuthToken\AuthKey.txt");
+
+                if (File.Exists(path))
+                {
+                    BearerToken = File.ReadAllText(path);
+                }
+
+                HttpWebRequest httpWebRequest = null;
+                httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+
+                httpWebRequest.Headers.Add("Authorization", "Bearer " + BearerToken);
+                httpWebRequest.Method = "POST";
+                httpWebRequest.ContentType = "application/json";
+
+
+                byte[] byteArray = Encoding.UTF8.GetBytes(Request);
+                httpWebRequest.ContentLength = byteArray.Length;
+                
+
+                Stream dataStream = httpWebRequest.GetRequestStream();
+                // Write the data to the request stream.
+                dataStream.Write(byteArray, 0, byteArray.Length);
+                // Close the Stream object.
+                dataStream.Close();
+                
+                WebResponse response = httpWebRequest.GetResponse();
+
+                using (dataStream = response.GetResponseStream())
+                {
+
+                    StreamReader reader = new StreamReader(dataStream);
+                    // Read the content.
+                    Json = reader.ReadToEnd();
+
+                    /////working as required
+                    var jsonData = JObject.Parse(Json);
+                    var objJsonData = jsonData["body"]["Data"];
+                    IEnumerable<Holding> result = JsonConvert.DeserializeObject<IEnumerable<Holding>>(objJsonData.ToString());
+                    var valuesList = result.Select(v => v.AvgRate).FirstOrDefault();
+
+                    ////working in required for json array
+                    ////var jsonData = JObject.Parse(Json);
+                    ////var objJsonData = jsonData["body"]["Data"];
+                    ////IEnumerable<MarketSts> result = JsonConvert.DeserializeObject<IEnumerable<MarketSts>>(objJsonData.ToString());
+                    ////var valuesList = result.ToList();
+                    ////foreach (var member in valuesList)
+                    ////{
+                    ////    Console.WriteLine(member.Exch);
+                    ////    Console.WriteLine(member.ExchDescription);
+                    ////    Console.WriteLine(member.ExchType);
+                    ////    Console.WriteLine(member.MarketStatus);
+                    ////}
+                }
+
+            }
+            catch (Exception ex)
+            {
+                 throw ex;
+            }
+            return Json;
+
+        }
+
+        public static string SendApiRequestMarketStatus(string url, string Request, string Type = "Openapi")
+        {
+            string Json = "";
+            string BearerToken = "";
+            try
+            {
+                string path = Path.Combine(Directory.GetCurrentDirectory(), @"AuthToken\AuthKey.txt");
+
+                if (File.Exists(path))
+                {
+                    BearerToken = File.ReadAllText(path);
+                }
+
+                HttpWebRequest httpWebRequest = null;
+                httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+
+                httpWebRequest.Headers.Add("Authorization", "Bearer " + BearerToken);
+                httpWebRequest.Method = "POST";
+                httpWebRequest.ContentType = "application/json";
+
+
+                byte[] byteArray = Encoding.UTF8.GetBytes(Request);
+                httpWebRequest.ContentLength = byteArray.Length;
+
+
+                Stream dataStream = httpWebRequest.GetRequestStream();
+                // Write the data to the request stream.
+                dataStream.Write(byteArray, 0, byteArray.Length);
+                // Close the Stream object.
+                dataStream.Close();
+
+                WebResponse response = httpWebRequest.GetResponse();
+
+                using (dataStream = response.GetResponseStream())
+                {
+
+                    StreamReader reader = new StreamReader(dataStream);
+                    // Read the content.
+                    Json = reader.ReadToEnd();
+
+                    /////working as required
+                    var jsonData = JObject.Parse(Json);
+                    //var objJsonData = jsonData["body"]["Data"];
+                    //IEnumerable<Holding> result = JsonConvert.DeserializeObject<IEnumerable<Holding>>(objJsonData.ToString());
+                    //var valuesList = result.Select(v => v.AvgRate).FirstOrDefault();
+
+                    ////working in required for json array
+                    ////var jsonData = JObject.Parse(Json);
+                    ////var objJsonData = jsonData["body"]["Data"];
+                    ////IEnumerable<MarketSts> result = JsonConvert.DeserializeObject<IEnumerable<MarketSts>>(objJsonData.ToString());
+                    ////var valuesList = result.ToList();
+                    ////foreach (var member in valuesList)
+                    ////{
+                    ////    Console.WriteLine(member.Exch);
+                    ////    Console.WriteLine(member.ExchDescription);
+                    ////    Console.WriteLine(member.ExchType);
+                    ////    Console.WriteLine(member.MarketStatus);
+                    ////}
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return Json;
+
+        }
+
+        #endregion Testing Api Only
+        public static string SendApiRequestCookies(string url, string Request, string Type = "Openapi")
+        {
+            string strresponse = "";
+            try
+            {
+                HttpWebRequest httpWebRequest = null;
+                httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+                
+                httpWebRequest.Headers.Add("Authorization", "Bearer " + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjUwMDg0NzkwIiwicm9sZSI6IjIyNTU5IiwiU3RhdGUiOiIiLCJSZWRpcmVjdFNlcnZlciI6IkEiLCJuYmYiOjE3MTI3Mjg2MDMsImV4cCI6MTcxMjc3Mzc5OSwiaWF0IjoxNzEyNzI4NjAzfQ._yUkCRWzPvLIevA1FMdpvI8LOqJPiwC8i5nnE-hTxRQ");
+                httpWebRequest.Method = "POST";
+                httpWebRequest.ContentType = "application/json";
+               
+                byte[] byteArray = Encoding.UTF8.GetBytes(Request);
+                httpWebRequest.ContentLength = byteArray.Length;
+                
+                Stream dataStream = httpWebRequest.GetRequestStream();
+                // Write the data to the request stream.
+                dataStream.Write(byteArray, 0, byteArray.Length);
+                // Close the Stream object.
+                dataStream.Close();
+
+                WebResponse response = httpWebRequest.GetResponse();
+
+                using (dataStream = response.GetResponseStream())
+                {
+
+                    StreamReader reader = new StreamReader(dataStream);
+                    // Read the content.
+                    strresponse = reader.ReadToEnd();
+                }
+               
+            }
+            catch (Exception ex)
+            {
+                return "PostError:" + ex.Message;
+            }
+
+            return strresponse;
+
+        }
+
+        public static void SetCookies(string CookieName)
+        {
+            var value = CookieName.Split(';');
+
+            string Fivepaisacookieres = Array.Find(value, ele => ele.Contains("5paisacookie", StringComparison.Ordinal));
+           
+            if (!string.IsNullOrEmpty(Fivepaisacookieres))
+            {
+                var Fivepaisacookiefinal = Fivepaisacookieres.Split('=');
+                string FivepaisacookieCookieValue = "5paisacookie=" + Fivepaisacookiefinal[1];
+                AddDataToXML("5paisacookie", FivepaisacookieCookieValue);
+            }
+
+            string JwtTokencookieres = Array.Find(value, ele => ele.Contains("JwtToken", StringComparison.Ordinal));
+           
+            if (!string.IsNullOrEmpty(JwtTokencookieres))
+            {
+                var JwtTokencookiefinal = JwtTokencookieres.Split('=');
+                string JwtTokenCookieValue = JwtTokencookiefinal[1];
+                AddDataToXML("JwtToken", JwtTokenCookieValue);
+            }
+
+            string ASPXAUTHcookieres = Array.Find(value, ele => ele.Contains(".ASPXAUTH", StringComparison.Ordinal));
+
+            if (!string.IsNullOrEmpty(ASPXAUTHcookieres))
+            {
+                var ASPXAUTHcookiefinal = ASPXAUTHcookieres.Split('=');
+                string ASPXAUTHCookieValue = ASPXAUTHcookiefinal[1];
+                AddDataToXML("ASPXAUTH", ASPXAUTHCookieValue);
+            }
+        }
+
+        public static void AddDataToXML(string Name, string Value)
+        {
+            string xmlFilePath = Path.Combine(Directory.GetCurrentDirectory(), "CookiesFile.xml");
+            XmlTextReader xmlreader = new XmlTextReader(xmlFilePath);
+            DataSet ds = new DataSet();
+            ds.ReadXml(xmlreader);
+
+            DataRow row = ds.Tables[0].Select("Name = '"+ Name + "'").FirstOrDefault();
+
+            int xmlRow = ds.Tables[0].Rows.IndexOf(row);
+            ds.Tables[0].Rows[xmlRow]["Value"] = Value;
+            xmlreader.Close();
+
+            ds.WriteXml(xmlFilePath);
+
+        }
+
+        public static string GetCookiesByName(string Name)
+        {
+            string value = string.Empty;
+
+            string xmlFilePath = Path.Combine(Directory.GetCurrentDirectory(), "CookiesFile.xml");
+            XmlTextReader xmlreader = new XmlTextReader(xmlFilePath);
+            DataSet ds = new DataSet();
+            ds.ReadXml(xmlreader);
+
+            DataRow row = ds.Tables[0].Select("Name = '" + Name + "'").FirstOrDefault();
+
+            int xmlRow = ds.Tables[0].Rows.IndexOf(row);
+            value = Convert.ToString(ds.Tables[0].Rows[xmlRow]["Value"]);
+            xmlreader.Close();
+
+            return value;
+        }
+        
+    }
+}
---
+++ WebApi5Paisa/WebApi5Paisa/Controllers/FivepaisaApiController.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace WebApi5Paisa.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FivepaisaApiController : ControllerBase
+    {
+        private string _apiKey = "C67swLy6gPrdmUhNQA8JcrTRtPAvwDA5";
+        private string EncryptionKey= "cyJ5ThTtO5uM0OSxgEqtoVSYuUVUCshR";
+        private string encryptUserId= "7kSEQSA4FIS";
+        private string _clientCode = "50084790";
+        Token Token { get; set; }
+
+        private readonly JsonData _JsonData;
+        private readonly string  _OpenAPIURL,_Holding, _MarketStatus, _LoginCheck, _AuthToken;
+        public FivepaisaApiController(IConfiguration _iConfig)
+        {
+            var folderDetails = Path.Combine(Directory.GetCurrentDirectory(), "APICredentials.json");
+            var JSON = System.IO.File.ReadAllText(folderDetails);
+
+            _JsonData = JsonConvert.DeserializeObject<JsonData>(JSON);
+
+            _OpenAPIURL = _iConfig.GetValue<string>("APIDetails:OpenAPIURL");
+            _Holding = _OpenAPIURL + _iConfig.GetValue<string>("APIDetails:Holding");
+            _MarketStatus = _OpenAPIURL + _iConfig.GetValue<string>("APIDetails:MarketStatus");
+            _LoginCheck = _OpenAPIURL + _iConfig.GetValue<string>("APIDetails:TOTPLogin");
+            _AuthToken = _OpenAPIURL + _iConfig.GetValue<string>("APIDetails:AuthToken");
+        }
+        [HttpGet]
+        [Route("TOTPLogin")]
+        public OutputBaseClass TOTPLogin(string _TOTP)
+        {
+            OutputBaseClass res = new OutputBaseClass();
+            _JsonData.Head.requestCode = _JsonData.RequestCode.LoginCheck;
+
+            try
+            {
+                string _EmailId = "8850999153";
+                string _Pin = "030906";
+                TokenResponse agr = new TokenResponse();
+                string URL = _LoginCheck;
+                var dataStringSession = JsonConvert.SerializeObject(new
+                {
+                    head = new { Key = _apiKey },
+                    body = new { Email_ID = _EmailId, TOTP = _TOTP, PIN = _Pin }
+
+                });
+                var json = ApiRequest.SendApiRequestTOTPLogin(URL, dataStringSession);
+                agr = JsonConvert.DeserializeObject<TokenResponse>(json);
+                var reqtok = agr.body.RequestToken;
+                //GetOuthLogin(agr.body.RequestToken);
+
+                string URLtoken = _AuthToken;
+                var dataString = JsonConvert.SerializeObject(new
+                {
+                    head = new { Key = _apiKey },
+                    body = new { RequestToken = agr.body.RequestToken, EncryKey = EncryptionKey, UserId = encryptUserId }
+
+                });
+                var jsonData = ApiRequest.SendApiRequestGetOuthLogin(URLtoken, dataString);
+                agr = JsonConvert.DeserializeObject<TokenResponse>(jsonData);
+                if (agr.body.Status == "0")
+                {
+
+                    res.TokenResponse = agr.body;
+                    res.status = agr.body.Status;
+                    res.http_error = agr.body.Message;
+                    res.http_code = agr.errorcode;
+                    this.Token = agr.body;
+                }
+                else
+                {
+
+                    res.status = agr.body.Status;
+                    res.http_error = agr.body.Message;
+                }
+
+            }
+            catch (Exception ex)
+            {  
+                res.http_error = ex.Message;
+            }
+            return res;
+        }
+
+        [HttpGet]
+        [Route("Holding")]
+        public string Holding()
+        {
+            ResponseModel objResponseModel = new ResponseModel();
+            string response = "";
+            try
+            {
+                _JsonData.Head.requestCode = _JsonData.RequestCode.Holding;
+
+                var Request = JsonConvert.SerializeObject(new
+                {
+                    head = new { key = _apiKey },
+                    body = new { ClientCode = _clientCode }
+
+                });
+                String result = Request.Replace("^\"|\"$", "");
+
+                //response = ApiRequest.SendApiRequestCookies(_Holding, result);
+                response = ApiRequest.SendApiRequestHolding(_Holding, result);
+                
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            
+            return response;
+        }
+        [HttpGet]
+        [Route("MarketStatus")]
+        public string MarketStatus()
+        {
+            ResponseModel objResponseModel = new ResponseModel();
+            string response = "";
+            try
+            {
+                _JsonData.Head.requestCode = _JsonData.RequestCode.MarketStatus;
+
+                var Request = JsonConvert.SerializeObject(new
+                {
+                    head = new { key = _apiKey },
+                    body = new { ClientCode = _clientCode }
+
+                });
+                String result = Request.Replace("^\"|\"$", "");
+
+                //response = ApiRequest.SendApiRequestCookies(_Holding, result);
+                response = ApiRequest.SendApiRequestMarketStatus(_MarketStatus, result);
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return response;
+        }
+    }
+}
