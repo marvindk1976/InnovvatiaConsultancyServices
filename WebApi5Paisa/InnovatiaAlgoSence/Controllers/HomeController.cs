8e425c1f201e1534dc8631df7a971c18453bf6a8
SarveshInnovatiacs <Sarvesh@innovatiacs.com>
2024-04-19 20:56:44 +0530

    project include

---
+++ WebApi5Paisa/InnovatiaAlgoSence/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using InnovatiaAlgoSence.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi5Paisa.Models;
+
+namespace InnovatiaAlgoSence.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(Login model)
+        {
+            string username = "sa", password = "1234";
+            if (ModelState.IsValid)
+            {
+                if (username == model.UserName && password == model.Password)
+                {
+                    return RedirectToAction("Dashboard");
+                }
+            }
+            return RedirectToAction("Fail");
+        }
+        public IActionResult Success()
+        {
+            return View();
+        }
+
+        public IActionResult Fail()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
