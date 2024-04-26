4100d80012b0d42dd349b81984d2509cf2d67dd3
SarveshInnovatiacs <Sarvesh@innovatiacs.com>
2024-04-26 22:05:13 +0530

    updated

---
+++ WebApi5Paisa/NetTaskScheduler/POrderRequestApi.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetTaskScheduler
+{
+    public class POrderRequestApi
+    {
+        public async Task CallPOrderRequestApi(string json)
+        {
+            var builder = new ConfigurationBuilder()
+                           .SetBasePath(Directory.GetCurrentDirectory())
+                           .AddJsonFile("config.json", optional: false);
+
+            IConfiguration config = builder.Build();
+
+            var getJsonValue = config.GetSection("GetJsonValue").Get<GetJsonValue>();
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(getJsonValue.BaseUrl);
+            await client.PostAsync(getJsonValue.POrderUrl, new StringContent(json, Encoding.UTF8, "application/json"));
+            //if (response != null)
+            //{
+            //    Console.WriteLine(response.ToString());
+            //}
+        }
+    }
+
+    public class GetJsonValue
+    {
+        public string BaseUrl { get; set; }
+        public string POrderUrl { get; set; }
+    }
+}
--- WebApi5Paisa/NetTaskScheduler/Program.cs
+++ WebApi5Paisa/NetTaskScheduler/Program.cs
@@ -19,72 +19,79 @@ namespace NetTaskScheduler
 
         static void Main(string[] args)
         {
-            string qty = ""; string Price = ""; string OrderType = ""; string ScripData = ""; DateTime Fromdatetime;
-            var Todatetime = "";
+            ////string qty = ""; string Price = ""; string OrderType = ""; string ScripData = ""; DateTime Fromdatetime;
+            ////var Todatetime = "";
 
-            var row1 = "";
-            var row2 = "";
-            string inputData = "";
-            string path = Path.Combine(Directory.GetCurrentDirectory(), @"InputTextFile\InputData.txt");
-            List<string> file = File.ReadAllLines(path).ToList();
-            foreach (string line in file)
+            ////var row1 = "";
+            ////var row2 = "";
+            ////string inputData = "";
+            ////string path = Path.Combine(Directory.GetCurrentDirectory(), @"InputTextFile\InputData.txt");
+            ////List<string> file = File.ReadAllLines(path).ToList();
+            ////foreach (string line in file)
+            ////{
+            ////    string splitline = line;
+            ////    string[] ssize = splitline.Split(new char[0]);
+            ////    row1 = ssize[0];
+            ////    row2 = ssize[1];
+
+            ////    //// Write file using StreamWriter
+            ////    if (row1 == "IsSuccessfull" && row2 == "0")
+            ////    {
+            ////        break;
+            ////    }
+
+            ////    inputData += string.Concat(string.Concat(row2), ",");
+            ////}
+            ////if (row1 == "IsSuccessfull" && row2 == "0")
+            ////{
+
+            ////    ////execute
+            ////    //string FinalPath = @"C:\Sarvesh\R&D Project\ConsoleApp1\ConsoleApp1\FInputData\FinalInput.txt";
+            ////    string fullPath = Path.Combine(Directory.GetCurrentDirectory(), @"InputTextFile\FinalInput.txt");
+            ////    TextWriter tw = new StreamWriter(fullPath, false);
+            ////    tw.Write(string.Empty);
+            ////    tw.Close();
+
+            ////    using (StreamWriter writer = new StreamWriter(fullPath))
+            ////    {
+            ////        writer.WriteLine(inputData.TrimEnd(','));
+            ////    }
+
+            ////    /////replace text 
+            ////    // string fullPath2 = @"C:\Sarvesh\R&D Project\ConsoleApp1\ConsoleApp1\InputData.txt";
+            ////    string fullPath2 = Path.Combine(Directory.GetCurrentDirectory(), @"InputTextFile\InputData.txt");
+            ////    using (StreamReader reader = new StreamReader(fullPath2))
+            ////    {
+
+            ////        string content = reader.ReadToEnd();
+            ////        reader.Close();
+            ////        content = Regex.Replace(content, "IsSuccessfull	0", "IsSuccessfull	1");
+
+            ////        // Write the content back to the file
+            ////        using (StreamWriter writer = new StreamWriter(fullPath2))
+            ////        {
+            ////            writer.Write(content);
+            ////        }
+            ////    }
+            ////    var Host = new HostBuilder()
+            ////    .ConfigureHostConfiguration(hConfig => { })
+            ////    .ConfigureServices((context, services) =>
+            ////    {
+            ////        services.AddHostedService<MyProcessor>();
+            ////    }).UseConsoleLifetime().Build();
+            ////    Host.Run();
+            ////}
+            ////else
+            ////{
+            ////    Console.WriteLine("Request already Raised for this details.");
+            ////}
+            var Host = new HostBuilder()
+            .ConfigureHostConfiguration(hConfig => { })
+            .ConfigureServices((context, services) =>
             {
-                string splitline = line;
-                string[] ssize = splitline.Split(new char[0]);
-                row1 = ssize[0];
-                row2 = ssize[1];
-
-                //// Write file using StreamWriter
-                if (row1 == "IsSuccessfull" && row2 == "0")
-                {
-                    break;
-                }
-
-                inputData += string.Concat(string.Concat(row2), ",");
-            }
-            if (row1 == "IsSuccessfull" && row2 == "0")
-            {
-
-                ////execute
-                //string FinalPath = @"C:\Sarvesh\R&D Project\ConsoleApp1\ConsoleApp1\FInputData\FinalInput.txt";
-                string fullPath = Path.Combine(Directory.GetCurrentDirectory(), @"InputTextFile\FinalInput.txt");
-                TextWriter tw = new StreamWriter(fullPath, false);
-                tw.Write(string.Empty);
-                tw.Close();
-
-                using (StreamWriter writer = new StreamWriter(fullPath))
-                {
-                    writer.WriteLine(inputData.TrimEnd(','));
-                }
-
-                /////replace text 
-                // string fullPath2 = @"C:\Sarvesh\R&D Project\ConsoleApp1\ConsoleApp1\InputData.txt";
-                string fullPath2 = Path.Combine(Directory.GetCurrentDirectory(), @"InputTextFile\InputData.txt");
-                using (StreamReader reader = new StreamReader(fullPath2))
-                {
-
-                    string content = reader.ReadToEnd();
-                    reader.Close();
-                    content = Regex.Replace(content, "IsSuccessfull	0", "IsSuccessfull	1");
-
-                    // Write the content back to the file
-                    using (StreamWriter writer = new StreamWriter(fullPath2))
-                    {
-                        writer.Write(content);
-                    }
-                }
-                var Host = new HostBuilder()
-                .ConfigureHostConfiguration(hConfig => { })
-                .ConfigureServices((context, services) =>
-                {
-                    services.AddHostedService<MyProcessor>();
-                }).UseConsoleLifetime().Build();
-                Host.Run();
-            }
-            else
-            {
-                Console.WriteLine("Request already Raised for this details.");
-            }
+                services.AddHostedService<MyProcessor>();
+            }).UseConsoleLifetime().Build();
+            Host.Run();
 
         }
     }
@@ -104,68 +111,138 @@ namespace NetTaskScheduler
             while (!stoppingToken.IsCancellationRequested)
             {
 
-                string fullPath3 = Path.Combine(Directory.GetCurrentDirectory(), @"InputTextFile\FinalInput.txt");
-                string[] lines = File.ReadAllLines(fullPath3);
-                foreach (string line in lines)
+                //string qty = ""; string Price = ""; string OrderType = ""; string ScripData = ""; DateTime Fromdatetime;
+                //var Todatetime = "";
+
+                var row1 = "";
+                var row2 = "";
+                string inputData = "";
+                string path = Path.Combine(Directory.GetCurrentDirectory(), @"InputTextFile\InputData.txt");
+                List<string> file = File.ReadAllLines(path).ToList();
+                foreach (string line in file)
                 {
-                    //var qty = line.Split(1) ;
-                    // splitline.Split(new char[0])
-                    var myString = line.Split(',');
-                    Exchange = myString[0];
-                    ExchangeType = myString[1];
-                    Qty = myString[2];
-                    Price = myString[3];
-                    OrderType = myString[4];
-                    ScripData = myString[5];
-                    IsIntraday = Convert.ToBoolean(myString[6]);
-                    DisQty = Convert.ToInt32(myString[7]);
-                    StopLossPrice = Convert.ToInt32(myString[8]);
-                    Fromdatetime = DateTime.Parse(myString[9]);
-                    Todatetime = DateTime.Parse(myString[10]);
-                }
+                    string splitline = line;
+                    string[] ssize = splitline.Split(new char[0]);
+                    row1 = ssize[0];
+                    row2 = ssize[1];
 
-
-                DateTime frmdatetime = DateTime.Parse(Fromdatetime.ToString("MM/dd/yyyy HH:mm:ss"));
-                DateTime todatetime = DateTime.Parse(Todatetime.ToString("MM/dd/yyyy HH:mm:ss"));
-
-                DateTime frmSysDate = DateTime.Parse(DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss"));
-                DateTime toSysDate = DateTime.Parse(DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss"));
-                //if (fromDate >= frmSysDate && )
-                if (frmSysDate >= frmdatetime && toSysDate <= todatetime)
-                {
-                    
-                    var Request = JsonConvert.SerializeObject(new
+                    //// Write file using StreamWriter
+                    if (row1 == "IsSuccessfull" && row2 == "0")
                     {
-                        head = new { key = "C67swLy6gPrdmUhNQA8JcrTRtPAvwDA5" },
-                        body = new
-                        {
-                            ClientCode = "50084790",
-                            Exchange = Exchange,
-                            ExchangeType = ExchangeType,
-                            Qty = Qty,
-                            Price = Price,
-                            OrderType = OrderType,
-                            ScripData = ScripData,
-                            IsIntraday = IsIntraday,
-                            DisQty = DisQty,
-                            StopLossPrice = StopLossPrice
-                        }
-
-                    });
-                    String result = Request.Replace("^\"|\"$", "");
-                    HttpClient client = new HttpClient();
-                    client.BaseAddress = new Uri("http://localhost:26328/");
-                    var response = await client.PostAsync("api/FivepaisaApi/POrderRequest", new StringContent(result, Encoding.UTF8, "application/json"));
-                    if (response != null)
-                    {
-                        Console.WriteLine(response.ToString());
+                        break;
                     }
-                    Console.WriteLine($"The Current date is {DateTime.Now}");
-                    await Task.Delay(10000);
+
+                    inputData += string.Concat(string.Concat(row2), ",");
+                }
+                if (row1 == "IsSuccessfull" && row2 == "0")
+                {
+
+                    ////execute
+                    //string FinalPath = @"C:\Sarvesh\R&D Project\ConsoleApp1\ConsoleApp1\FInputData\FinalInput.txt";
+                    string fullPath = Path.Combine(Directory.GetCurrentDirectory(), @"InputTextFile\FinalInput.txt");
+                    TextWriter tw = new StreamWriter(fullPath, false);
+                    tw.Write(string.Empty);
+                    tw.Close();
+
+                    using (StreamWriter writer = new StreamWriter(fullPath))
+                    {
+                        writer.WriteLine(inputData.TrimEnd(','));
+                    }
+
+                    /////replace text 
+                    // string fullPath2 = @"C:\Sarvesh\R&D Project\ConsoleApp1\ConsoleApp1\InputData.txt";
+                    string fullPath2 = Path.Combine(Directory.GetCurrentDirectory(), @"InputTextFile\InputData.txt");
+                    using (StreamReader reader = new StreamReader(fullPath2))
+                    {
+
+                        string content = reader.ReadToEnd();
+                        reader.Close();
+                        content = Regex.Replace(content, "IsSuccessfull	0", "IsSuccessfull	1");
+
+                        // Write the content back to the file
+                        using (StreamWriter writer = new StreamWriter(fullPath2))
+                        {
+                            writer.Write(content);
+                        }
+                    }
+                    //}
+                    //else
+                    //{
+                    //    Console.WriteLine("Request already Raised for this details.");
+                    //}
+
+                    string fullPath3 = Path.Combine(Directory.GetCurrentDirectory(), @"InputTextFile\FinalInput.txt");
+                    string[] lines = File.ReadAllLines(fullPath3);
+                    foreach (string line in lines)
+                    {
+                        //var qty = line.Split(1) ;
+                        // splitline.Split(new char[0])
+                        var myString = line.Split(',');
+                        Exchange = myString[0];
+                        ExchangeType = myString[1];
+                        Qty = myString[2];
+                        Price = myString[3];
+                        OrderType = myString[4];
+                        ScripData = myString[5];
+                        IsIntraday = Convert.ToBoolean(myString[6]);
+                        DisQty = Convert.ToInt32(myString[7]);
+                        StopLossPrice = Convert.ToInt32(myString[8]);
+                        Fromdatetime = DateTime.Parse(myString[9]);
+                        Todatetime = DateTime.Parse(myString[10]);
+                    }
+
+
+                    DateTime frmdatetime = DateTime.Parse(Fromdatetime.ToString("MM/dd/yyyy HH:mm:ss"));
+                    DateTime todatetime = DateTime.Parse(Todatetime.ToString("MM/dd/yyyy HH:mm:ss"));
+
+                    DateTime frmSysDate = DateTime.Parse(DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss"));
+                    DateTime toSysDate = DateTime.Parse(DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss"));
+                    //if (fromDate >= frmSysDate && )
+                    if (frmSysDate >= frmdatetime && toSysDate <= todatetime)
+                    {
+
+                        var Request = JsonConvert.SerializeObject(new
+                        {
+                            head = new { key = "C67swLy6gPrdmUhNQA8JcrTRtPAvwDA5" },
+                            body = new
+                            {
+                                ClientCode = "50084790",
+                                Exchange = Exchange,
+                                ExchangeType = ExchangeType,
+                                Qty = Qty,
+                                Price = Price,
+                                OrderType = OrderType,
+                                ScripData = ScripData,
+                                IsIntraday = IsIntraday,
+                                DisQty = DisQty,
+                                StopLossPrice = StopLossPrice
+                            }
+
+                        });
+                        String result = Request.Replace("^\"|\"$", "");
+                        POrderRequestApi pOrderRequestApi = new POrderRequestApi();
+                        var responseData = pOrderRequestApi.CallPOrderRequestApi(result);
+
+                        //HttpClient client = new HttpClient();
+                        //client.BaseAddress = new Uri("http://localhost:26328/");
+                        //var response = await client.PostAsync("api/FivepaisaApi/POrderRequest", new StringContent(result, Encoding.UTF8, "application/json"));
+                        //if (response != null)
+                        //{ 
+                        //    Console.WriteLine(response.ToString());
+                        //}
+                        Console.WriteLine($"The Place Order request is processed Successfully...! {DateTime.Now}");
+                        //Thread.Sleep(10000);
+                        await Task.Delay(10000);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Date time is not Valid for Place Order request...! {DateTime.Now}");
+                        await Task.Delay(10000);
+                    }
                 }
                 else
                 {
-                    Console.WriteLine($"The Current date time is {DateTime.Now} not Valid");
+                    Console.WriteLine($"The Place Order request is Already processed.Modify the inputData { DateTime.Now}");
                     await Task.Delay(10000);
                 }
             }
